1c316f4b37fc4e57015cb30230a420f61c3a5a79
Riley Parker <riley.dexter922@gmail.com>
2026-04-29 14:57:26 -0400

    Better Implementation

--- utils.py
+++ utils.py
@@ -1,21 +1,22 @@
 import functools
 import pandas as pd
 
-# Custom Exception
+
 class InvalidDataError(Exception):
-    """Raised when weather or energy values are physically impossible."""
+    """Raised when the input data has values that should not be possible."""
     pass
 
-# Decorator for logging
+
 def logger(func):
+    """Small logger so we can see when important steps are running."""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        print(f"[LOG] Executing {func.__name__}...")
+        print(f"[LOG] Running {func.__name__}...")
         return func(*args, **kwargs)
     return wrapper
 
-# Generator for memory efficiency
+
 def data_chunk_generator(file_path, chunk_size=1000):
-    """Yields chunks of the dataset to save RAM."""
+    """Read a CSV file in smaller pieces instead of loading everything at once."""
     for chunk in pd.read_csv(file_path, chunksize=chunk_size):
         yield chunk
